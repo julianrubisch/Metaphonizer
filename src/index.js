209d62546240761ed3bed74896e2c3e666558635
Julian Rubisch <julian@julianrubisch.at>
2019-03-29 10:43:57 +0100

    Extract configureStore and use redux devtools #2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './reducers';
+import configureStore from './configureStore';
 
-const store = createStore(rootReducer);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
