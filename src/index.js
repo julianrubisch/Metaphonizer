9f7a6cd939c385bff0427c081c90e6fdde4b4429
Julian Rubisch <julian@julianrubisch.at>
2019-03-29 11:02:43 +0100

    Add router setup #6

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
 import './index.css';
-import App from './components/App';
+import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore';
 
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>, 
+  <Root store={store}/>,
   document.getElementById('root')
 );
 
