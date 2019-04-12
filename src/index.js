7962d4b06e6527ef41623e05a775edbd867baf77
Julian Rubisch <julian@julianrubisch.at>
2019-04-12 11:00:26 +0200

    Activate service worker

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import Root from './components/Root';
-import * as serviceWorker from './serviceWorker';
-import configureStore from './configureStore';
+import "./index.css";
+import Root from "./components/Root";
+import * as serviceWorker from "./serviceWorker";
+import configureStore from "./configureStore";
 
 const store = configureStore();
 
-ReactDOM.render(
-  <Root store={store}/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
