e9055380215e1c188182dd1a6b8195526b28f417
Julian Rubisch <julian@julianrubisch.at>
2019-04-14 11:52:25 +0200

    Move test to __tests__ folder

--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-testing-library";
-import App from "./App";
+import App from "../App";
 
 it("renders welcome message", () => {
   const { getByText } = render(<App />);
