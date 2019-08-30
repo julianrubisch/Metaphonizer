772dc7a5c346bc4ce3d305be9377bf68b78f5462
Julian Rubisch <julian@julianrubisch.at>
2019-08-30 13:04:28 +0200

    Simple drag and drop import with dropzone
    MTPH-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import { useDropzone } from "react-dropzone";
+
+const App = () => {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag n drop some files here, or click to select files</p>
       </div>
-    );
-  }
-}
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+};
 
 export default App;
